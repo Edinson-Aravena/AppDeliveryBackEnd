06bec75c7a3b096d27e96783920833db8d4c9fa5
graciany <gr4ciany@gmail.com>
2025-02-12 21:35:08 -0300

    JSON WEB TOKEN

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secretOrKey : 'KhTpYJ0havoagCoj+7vRCUmuNidg/wqWSzAUZBFFPW7hMlWcgGRAGUu8rZnrLSG3GterLbhTg+5T3KxXIGe/rbZPeHWVoxdeKROrBFfqJyjhVqRWwRq1/ECPCYKS0HoIXS3P/XVxymLYjRxgYnxENiw8EHqiaCKvfY/VBTT0jLH1SyvRClk6TDSDpZ4pzJyk'
+}
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = http.createServer(app);
 
 const logger = require('morgan');
 const cors = require('cors');
+const passport = require('passport')
 
 const myIP = require('./getMyIP')
 
@@ -24,6 +25,10 @@ app.use(express.urlencoded({
 }))
 
 app.use(cors())
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.disable('x-powered-by');
 
